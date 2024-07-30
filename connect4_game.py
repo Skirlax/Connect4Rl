0e5de5ff1711bd4dbbe9e33d2c53e3efe1cfcb66
skyr <skirlaxxx@gmail.com>
2024-07-30 13:39:34 +0200

    Revert "Bug fixes and option to get human input."
    This reverts commit 5e8bbf33db006c6e336af133c8a8171a9cd6d1e3.

--- connect4_game.py
+++ connect4_game.py
@@ -52,9 +52,11 @@ class Connect4:
 
     def check_win(self, player: int, board: np.ndarray):
         """
-        Check if player has won the game. This method essentially performs a 2d convolution (without the summing) and
-        on each stride, checks if the player has won. This makes the method player invariant and board size invariant
-        as well as efficient. :param player: :param board: :return:
+        Check if player has won the game. This method essentially performs a 2d convolution (without the summing) and on each stride, check if the player has won.
+        This makes the method player invariant and board size invariant as well as efficient.
+        :param player:
+        :param board:
+        :return:
         """
         w = np.ones((self.num_to_win, self.num_to_win))
         # + 1 because the start position of the filter also counts.
@@ -107,7 +109,7 @@ class Connect4:
         board = self.play(action, player, board)
         result = self.game_result(player, board)
         done = result is not None
-        reward = 1 if done else 0
+        reward = result if done else 0
         return board, reward, done
 
     def get_human_input(self, board: np.ndarray):
